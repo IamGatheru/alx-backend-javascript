17e10b6ae48427ff40d836c27e6a6bfd09179898
IamGatheru <davidgatheru490@gmail.com>
2024-10-09 21:14:33 +0300

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a currency');
+    }
+    this._currency = currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  /**
+   * static method convertPrice.
+   *
+   * @param {string} amount - The amount.
+   * @param {string} conversionRate - The conversionRate.
+   */
+  static conversionPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
